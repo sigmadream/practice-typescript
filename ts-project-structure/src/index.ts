b631b43c9a77c11c997dea34d92da9e4912b2b2a
Sangkon Han <sigmadream@gmail.com>
2021-11-24 00:28:33 +0900

    Google Map API를 사용한 간단한 예제

--- ts-project-structure/src/index.ts
+++ ts-project-structure/src/index.ts
@@ -1,5 +1,10 @@
+import { Company } from './Company';
 import { User } from './User';
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-console.log(user);
+customMap.addCompanyMarker(company);
+customMap.addUserMarker(user);
