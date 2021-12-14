5898018b69eb90126e7ae2de59cb66793225a111
Sangkon Han <sigmadream@gmail.com>
2021-12-15 02:59:48 +0900

    // WIP
    * CSV 파일 구조 분석

--- ts-stats/src/index.ts
+++ ts-stats/src/index.ts
@@ -1,7 +1,24 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8',
-});
+// 10/08/2018,Man United,Leicester,2,1,H,A Marriner
 
-console.log(matches);
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
