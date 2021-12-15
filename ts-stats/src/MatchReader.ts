9268cc0bff2a827238ad928f7255964d7556e850
Sangkon, Han <sigmadream@gmail.com>
2021-12-16 02:54:17 +0900

    CSV 파싱 모듈 작성 (#3)
    * CSV 파일을 파싱해서 HTML로 결과를 출력

---
+++ ts-stats/src/MatchReader.ts
@@ -0,0 +1,35 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+import { MatchData } from './MatchData';
+import { CsvFileReader } from './CsvFileReader';
+
+interface DataReader {
+  data: string[][];
+
+  read(): void;
+}
+
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename));
+  }
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult, // 'H', 'A', 'D'
+        row[6],
+      ];
+    });
+  }
+}
