63b23311af5f4a51306f5089e656748b734a7e5d
Sangkon Han <sigmadream@gmail.com>
2021-12-16 02:49:02 +0900

    // WIP
    * 리팩토링

--- ts-stats/src/index.ts
+++ ts-stats/src/index.ts
@@ -1,17 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Crystal Palace');
 
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
